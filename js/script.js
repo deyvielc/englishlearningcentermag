6ce375852083c14b2c7a1f3bade0dcf5c9e422cf
deyvielc <managuaelc@gmail.com>
2024-11-21 22:02:44 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Script for responsive menu
+const mobileMenu = document.getElementById("mobile-menu");
+const navList = document.querySelector(".nav-list");
+
+mobileMenu.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
