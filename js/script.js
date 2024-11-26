263fe4ef44263237bc1dbfc3d3e052d0a0591d13
deyvielc <managuaelc@gmail.com>
2024-11-26 09:15:12 -0600

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Script for responsive menu
-const mobileMenu = document.getElementById("mobile-menu");
-const navList = document.querySelector(".nav-list");
-
-mobileMenu.addEventListener("click", () => {
-  navList.classList.toggle("active");
-});
